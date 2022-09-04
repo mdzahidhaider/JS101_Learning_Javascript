10c51d54815086ac10857b2a631054e54340930f
mdzhidhaider <mdzahidhaider@gmail.com>
2022-09-04 16:40:37 +0530

    4th Finally

---
+++ day4/p4.js
@@ -0,0 +1,8 @@
+// Problem 4: Given 3 numbers (all different values), print which is greatest
+
+var a=5
+var b=8
+var c=7
+
+a>b && a<c ? console.log(a,"is greater than",b, "and",c):
+     b>c && b>a ? console.log(b,"is greater than",c, "and",a):console.log(c,"is greater than",b, "and",a)                  
---
+++ day4/p6.js
@@ -0,0 +1,12 @@
+// Problem 5: Given the days of the week in short format "Sun", "Mon" ... print in long format "Sunday", "Monday", ...
+
+var day="Mon"
+
+day=="Mon" ? console.log("Monday"):
+  day=="Tues" ? console.lg("Tuesday"):
+  day=="Wed" ? console.log("Wednesday"):
+  day=="Thurs" ? console.log("Thursday"):
+  day=="Fri" ? console.log("Friday"):
+  day=="Sat" ? console.log("Saturday"):
+  day=="Sun" ? console.log("Sunday"):
+  console.log("NA")
