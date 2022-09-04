344f319a3d3e9eb5cbafad05ebb70fa5896bca12
mdzhidhaider <mdzahidhaider@gmail.com>
2022-09-04 16:29:47 +0530

    2nd Finally

---
+++ day2/q1.js
@@ -0,0 +1,39 @@
+// # Problem 1 : Find Product
+
+// ### Description
+
+// - You have to take given five numbers stored in variables with the following names
+    
+//     ```
+//     one, two, three, four, five
+//     ```
+    
+// - Find the product of the five values, and print it
+
+// ### Input
+
+// The first and the only line of the input contains five values, stored in the variables`one, two, three, four, five`
+
+// ### Output
+
+// Print the product of the five values stored in the variables`one, two, three, four, five`
+
+// ### Sample Input
+
+// ```jsx
+// 1 2 3 4 5
+// ```
+
+// ### Sample Output
+
+// ```jsx
+// 120
+// ```
+
+// ### Hint
+
+// In the sample test case, the values stored in the variables are`one = 1, two = 2, three = 3, four = 4, five= 5`
+
+// The sum of the values is`1 * 2 * 3 * 4 * 5 = 120`, which is the required sum
+
+console.log(1 * 2 * 3 * 4 * 5);
